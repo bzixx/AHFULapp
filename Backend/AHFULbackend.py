286e5621518f6c76c189d04c47345876a3e0c9dd
ElijahTurany <turanye6132@my.uwstout.edu>
2026-02-13 16:32:02 -0600

    Loading env vals

--- Backend/AHFULbackend.py
+++ Backend/AHFULbackend.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+load_dotenv()
 
 @app.route("/")
 def hello_world():
---
+++ Backend/config.py
@@ -0,0 +1,2 @@
+MONGODB_URI=mongodb+srv://AHFULClientDBUser:Planet!Blood!Limited!Bound6@ahful.dicih8m.mongodb.net/?appName=AHFUL
+MONGODB_DB=AHFUL
