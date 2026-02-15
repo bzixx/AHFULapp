d4e07e0def0a298e45ad7fc88b73068ac8611994
AuGust Ringelstetter <McGregor2kx@gmail.com>
2026-02-15 06:00:26 -0600

    Oops forgot to stage these iin previosu commit

--- Backend/AHFULbackend.py
+++ Backend/AHFULbackend.py
@@ -1,19 +1,33 @@
-from flask import Flask #Import Main Flask application class
-from services.MongoDriver import MongoDriver #[Local] Import MongoDB driver function
+from flask import Flask, current_app #Import Main Flask application class
+from services.userDriver import UserDriver #[Local] Import UserDriver for user DB operations
+from apiRoutes.userRoutes import userRouteBlueprint #[Local] Import User API routes as a Flask Blueprint
 
-# Create Flask application instance we will use to run the Backend server and handle requests
-app = Flask(__name__)
+from dotenv import load_dotenv
 
+
+def create_app():
+    app = Flask(__name__)
+    # Create Flask application instance we will use to run the Backend server and handle requests
+
+    # Load environment variables from .env file At Main Level
+    load_dotenv()
+
+    app.register_blueprint(userRouteBlueprint, url_prefix="/api/users")
+
+
+    
+    return app
 
 # Define a route for the root URL ("/") that returns a simple "Hello, World!" message when accessed
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @current_app.route("/")
+# def hello_world():
+#     return "<h1>Hello, World!</h1><h3>Welcome to the AHFUL Backend Server.</h3> <p>It's great here.</p>"
 
 #Test route to check if we can connect to MongoDB, will return the result of the returns a success message if successful
-@app.route("/testdb")
-def do_DB_test():
-    return MongoDriver()
+# @app.route("/testdb")
+# def do_DB_test():
+#     # return mongoDriver()
+
 
 
 
--- Backend/services/MongoDriver.py
+++ Backend/services/MongoDriver.py
@@ -1,32 +1,40 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
-from dotenv import load_dotenv
+from flask import current_app
 import os
 
-
-def MongoDriver():
-    load_dotenv()
+#Main function to connect to MongoDB, will return a success message if successful, 
+# and print the names of the collections in the database and the documents in the 
+# user collection for testing purposes
+def getMongoClient():
 
     uri = os.getenv("MONGODB_URI")
 
+    #Error handling for missing environment variable
+    if not uri:
+        raise RuntimeError("MongoDB connection failed: MONGODB_URI not found in environment.")
+
     # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'))
-
-    appDataDB = client["appData"]
-
-    print(appDataDB.list_collection_names())
-    userCollection = appDataDB['user']
-    len = userCollection.count_documents({})
-    for i in range(0, len):
-        print(userCollection.find_one({"userId": i}))
+    ahfulMongoDBClient = MongoClient(uri, server_api=ServerApi('1'))
 
     # Send a ping to confirm a successful connection
     try:
-        client.admin.command('ping')
+        ahfulMongoDBClient.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
 
-    return "MongoDB connection successful"
-    
+    return ahfulMongoDBClient
+
+def getMongoDatabase():
+    ahfulMongoDBClient = getMongoClient()
+    return ahfulMongoDBClient["appData"]
+
+
+
+def killMongoClient():
+    # This function is a placeholder for any cleanup operations needed for the MongoDB connection
+    openMongoDriver = current_app.config.get('ahfulMongoDBClient')
+    if openMongoDriver:
+        openMongoDriver.close()
+
