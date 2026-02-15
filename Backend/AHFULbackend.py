12a5acc76204f81da4ca1479ef6d6404dd08e67e
AuGust Ringelstetter <McGregor2kx@gmail.com>
2026-02-15 05:59:52 -0600

    Created Models(Objects), Routes(API Endpoints), and Services (Drivers) for each Collection in the backend and got the Users Main print all function to be working.  Need to troubleshoot other functions as AI was used to help scaffold the Blueprint design

--- Backend/AHFULbackend.py
+++ Backend/AHFULbackend.py
@@ -1,15 +1,21 @@
-from flask import Flask
-from services.MongoDriver import MongoDriver
+from flask import Flask #Import Main Flask application class
+from services.MongoDriver import MongoDriver #[Local] Import MongoDB driver function
 
+# Create Flask application instance we will use to run the Backend server and handle requests
 app = Flask(__name__)
 
+
+# Define a route for the root URL ("/") that returns a simple "Hello, World!" message when accessed
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+#Test route to check if we can connect to MongoDB, will return the result of the returns a success message if successful
 @app.route("/testdb")
 def do_DB_test():
     return MongoDriver()
+
+
 
 
     
