38c42c2fed3354b19dbcedb3d5dbb51d7395acf7
smitht8087 <tylers8567@gmail.com>
2026-02-16 16:29:40 -0600

    Updated color scheme/UI app colors

--- Frontend/src/layout.jsx
+++ Frontend/src/layout.jsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 export function Layout() {
     return (
         <>
-            <Navbar height = "90px"/>
+            <Navbar minHeight = "5vh"/>
             <main style={{ 
-                background: "#535353",
-                minHeight: "calc(100vh - 90px)",
+                background: "linear-gradient(0deg, #30364F, #3cb6b6)",
+                minHeight: "95vh",
                 display: "flex"
              }}>
             <Outlet/>
