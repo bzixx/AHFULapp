c3f69ece9ae9bd039e86787232cda84e60cef8cd
smitht8087 <tylers8567@gmail.com>
2026-02-16 16:29:40 -0600

    Updated color scheme/UI app colors

--- Frontend/src/components/navbar/navbar.jsx
+++ Frontend/src/components/navbar/navbar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-export function Navbar() {
+export function Navbar({ minHeight }) {
   return (
-    <nav>
+    <nav className = "navbar" style={{ minHeight }}>
       <Link to="/">Home</Link>
       {" | "}
       <Link to="/Login">Login</Link>
