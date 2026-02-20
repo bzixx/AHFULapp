54a7d60b198526d899a355a0f5d036d78603838b
Tony Skoglund <antyskoglund@gmail.com>
2026-02-20 15:44:09 -0600

    Commiting small to pull main

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 import "../../SiteStyles.css";
-import { Calendar } from "../../Components/Calendar/Calendar";
+import { Calendar } from "../../Components/Calendar/Calendar.jsx";
 export function Home() {
     return <>
     <div>Welcome to the home page!</div>
