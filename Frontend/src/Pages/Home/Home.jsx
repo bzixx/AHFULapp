bbb3f5fe3879b9985592eeccdeb276d7deff06c4
Tony Skoglund <antyskoglund@gmail.com>
2026-02-23 21:08:41 -0600

    Simple todo list functionality & committing for home pc

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import "../../SiteStyles.css";
+import { CalendarTodoSidebar } from "../../Components/CalendarTodoList/CalendarTodoSidebar.jsx";
 import { Calendar } from "../../Components/Calendar/Calendar.jsx";
 export function Home() {
     return (
