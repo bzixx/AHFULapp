e18ecb95950ce00744fc994134f9b7bc7c391338
ElijahTurany <turanye6132@my.uwstout.edu>
2026-02-25 14:58:38 -0600

    Verify object values

--- Backend/tests/test_routes.py
+++ Backend/tests/test_routes.py
@@ -11,8 +11,22 @@ def test_wont_fail():
 # def test_wont_pass():
 #     assert False
 
+
 def test_find_gym_by_id():
     oid = "699cff88400d9d43a32e924d"
-    gym = GymDriver.get_gym_by_id(oid)
-    print(gym)
+    gym, err = GymDriver.get_gym_by_id(oid)
+
+    if err is not None:
+        print(gym, err)
+
+    # Basic assertions
+    assert err is None
     assert gym is not None
+
+    # Assert values
+    assert gym.get("_id") == oid
+    assert gym.get("title") == "Downtown Fitness"
+    assert gym.get("address") == "123 Main St, Anytown, USA"
+    assert gym.get("cost") == 49.99
+    assert gym.get("link") == "https://examplegym.com"
+
