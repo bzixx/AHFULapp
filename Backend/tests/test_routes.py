f1a45015687e3137ab3dfcfc4738a5d73495b2d3
ElijahTurany <turanye6132@my.uwstout.edu>
2026-02-25 14:53:46 -0600

    Adding print output

--- Backend/tests/test_routes.py
+++ Backend/tests/test_routes.py
@@ -8,8 +8,8 @@ from Services.GymDriver import GymDriver
 def test_wont_fail():
     assert True
 
-def test_wont_pass():
-    assert False
+# def test_wont_pass():
+#     assert False
 
 def test_find_gym_by_id():
     oid = "699cff88400d9d43a32e924d"
