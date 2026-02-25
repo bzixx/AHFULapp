104f29d13e90ccce13c5209dfdca8d49ae0261a2
ElijahTurany <turanye6132@my.uwstout.edu>
2026-02-25 14:22:58 -0600

    Pass and fail test?

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,15 +2,14 @@
 import os
 import requests
 from bson import ObjectId
-#from Backend.Services.MongoDriver import getMongoDatabase
 
-#ahfulAppDataDB = getMongoDatabase()
-#gymCollection = ahfulAppDataDB['gym']
-
-# def test_get_gym_by_id():
-#     doc_id = "699cff88400d9d43a32e924d"
-#     print(doc_id)
-#     assert True is not None
 
 def test_wont_fail():
+    assert True
+
+def test_wont_pass():
+    assert False
+
+def test_find_gym_by_id():
+    oid = "699cff88400d9d43a32e924d"
     assert True
