9135d4f44330fb6cfeb8c02e565265b35c264d8d
AuGust Ringelstetter <McGregor2kx@gmail.com>
2026-02-28 00:19:51 -0600

    Added Logout button

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Home.css";
 import "../../SiteStyles.css";
-import { CalendarTodoSidebar } from "../../Components/CalendarTodoList/CalendarTodoSidebar.jsx";
-import { Calendar } from "../../Components/Calendar/Calendar.jsx";
 export function Home() {
     return (
         <main className="gym-landing-root">
