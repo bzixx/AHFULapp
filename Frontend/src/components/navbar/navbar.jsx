0c061a258474d586c369d7eb41c0c433627e5f30
Tony-Skog <antyskoglund@gmail.com>
2026-03-25 15:42:57 -0500

    Small route fix for tos

--- Frontend/src/components/navbar/navbar.jsx
+++ Frontend/src/components/navbar/navbar.jsx
@@ -10,7 +10,8 @@ export function Navbar({ minHeight }) {
       <Link to="/">Dashboard Home</Link>
       
       {!isAuthenticated ? (
-        <Link to="/Login">Login</Link>
+        <Link to="/Login">Login</Link>,
+        <Link to="/TOS">Terms of Service</Link>
       ) : (
         <>
           <Link to="/WorkoutLogger">Workout Logger</Link>
