8f9461d6f3d7630161a5b3c0cef1f70f5afd10c8
AuGust Ringelstetter <McGregor2kx@gmail.com>
2026-04-08 23:47:26 -0500

    - Updated Requirements - Also update settings to actually save and cahedk that it saves as expected.

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { updateSetting } from "../../Pages/Settings/SettingsSlice";
+import { updateUserSettings } from "../../QueryFunctions";
 import "./Header.css";
 import "../../Stylesheets/Themes/Lightmode.css";
 import "../../Stylesheets/Themes/Darkmode.css";
@@ -8,10 +9,17 @@ import "../../Stylesheets/Themes/Darkmode.css";
 export function Header({ onMenuToggle = null, isMenuOpen = false }) {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.setting?.theme || "Light");
+  const user = useSelector((state) => state.auth.user);
 
   const handleThemeToggle = () => {
     const newTheme = theme === "Light" ? "Dark" : "Light";
     dispatch(updateSetting({ key: "theme", value: newTheme }));
+    
+    if (user && user._id) {
+      updateUserSettings(user._id, { 
+        displayMode: newTheme === "Dark" ? "dark" : "light" 
+      }).catch(err => console.error("Failed to save theme:", err));
+    }
   };
 
   return (
