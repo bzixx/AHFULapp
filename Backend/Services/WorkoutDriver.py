92d7d732dd03156ed06307395ec4c58df832a447
ElijahTurany <turanye6132@my.uwstout.edu>
2026-04-27 13:32:32 -0500

    workout null gym updates, remove driver check (should never pass)

--- Backend/Services/WorkoutDriver.py
+++ Backend/Services/WorkoutDriver.py
@@ -82,9 +82,8 @@ class WorkoutDriver:
             "startTime": int(startTime),
             "endTime": int(endTime),
             "template": False,
+            "gym_id": ObjectId(gym_id)
         }
-        if gym_id:
-            workout_data["gym_id"] = ObjectId(gym_id)
 
         try:
             response = WorkoutObject.create(workout_data)
@@ -205,6 +204,8 @@ class WorkoutDriver:
         
         if updates.get("gym_id"):
             updates["gym_id"] = ObjectId(updates["gym_id"])
+        else:
+            updates["gym_id"] = 
 
         # Allowed fields to update
         allowed_fields = {
